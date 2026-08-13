810c84e5338d6749869c86c54841dc688890cec8
Letícia Silva Pedroso <l.spedroso77@gmail.com>
2026-08-13 15:07:54 -0300

    Personalizando o Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import foto from '../assets/foto2.png';
+export default function Hero() {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <div className="hero-foto">
+          <img src={foto} alt="Foto de Letícia" />
+        </div>
+
+        <div className="hero-text">
+
+        <p className="hero-greeting">
+          Hello, world!
+          <span className="typing-cursor">|</span>
+        </p>
+
+        <h1 className="hero-name">
+          Eu sou a Letícia
+        </h1>
+
+        <h2 className="hero-role">
+          Software Engineering Student
+        </h2>
+
+        <p className="hero-description">
+          Desenvolvedora em formação, apaixonada por tecnologia,
+          desenvolvimento web e por transformar ideias em projetos reais.
+        </p>
+
+        <button className="hero-button">
+          Ver meus projetos
+        </button>
+        </div>
+
+      </div>
+    </main>
+  )
+}
